11bf7f1ccce2cd680390929f8aea17fe5ae89580
Esteban Ordonez-Benavides <estebanusco@gmail.com>
2024-01-24 15:48:33 -0800

    workshop done

---
+++ block07workshop.js
@@ -0,0 +1,15 @@
+/* I will make the variable messageString by declaring it and putting the message from the instructuions*/
+let messageString = "You have received this message because you have been chosen to open an important vault.Here is the secret combination"
+
+// I will make x,y,z the variables for the vault combination
+let x = 5 * 2
+let y = x * 4
+let z = 78 / 2
+
+//i will turn all the number variable into one variable that is the combination
+let combination = [x, y, z]
+
+//I will turn the messageString variable and combination variable to 1 variable named "message"
+let message = messageString + ' ' + combination
+//i will log the message
+alert(message)
